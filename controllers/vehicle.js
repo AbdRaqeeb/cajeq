ef5bd3057c75247bdb33599a13e61d38c7da2747
Abdur-Raqeeb Ajao <ajaorqb@gmail.com>
2020-12-28 11:09:13 +0100

    Feat: added vehicle images upload controller

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -11,6 +11,38 @@ import geocoder from "../utils/geocoder.js";
  * @access  Private
  * */
 export const addVehicle = asyncHandler(async (req, res, next) => {
+    req.body.user = req.user.id;
+
+    let vehicle = await Vehicle.create(req.body);
+
+    vehicle = await Vehicle.findById(vehicle._id).populate('user', 'name email phone');
+
+    res.status(201).json({
+        success: true,
+        data: vehicle
+    })
+});
+
+/**
+ * @desc    Add vehicle images
+ * @route   PUT /api/v1/vehicles/:id
+ * @access  Private
+ * */
+export const addVehicleImages = asyncHandler(async (req, res, next) => {
+    let vehicle = await Vehicle.findById(req.params.id);
+
+    if (!vehicle) {
+        return next(
+            new ErrorResponse(`Vehicle with ID ${req.params.id} not found`, 404)
+        );
+    }
+
+    if (req.user.id !== vehicle.user.toString() && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`User not allowed to update vehicle`, 405)
+        );
+    }
+
     if (!req.files || req.files.length < 2) {
         return next(
             new ErrorResponse('Please upload vehicle images. Two or more', 400)
@@ -20,15 +52,17 @@ export const addVehicle = asyncHandler(async (req, res, next) => {
     // Upload image or images
     const images = await uploadImages(req.files.images, folders.cars, 'owner');
 
-    req.body.user = req.user.id;
-    req.body.images = images;
+    vehicle = await Vehicle.findByIdAndUpdate(req.params.id, {images}, {
+        new: true,
+        runValidators: false
+    });
 
-    const vehicle = await Vehicle.create(req.body);
+    vehicle = await Vehicle.findById(vehicle._id).populate('user', 'name email phone');
 
-    res.status(201).json({
+    res.status(200).json({
         success: true,
         data: vehicle
-    })
+    });
 });
 
 /**
